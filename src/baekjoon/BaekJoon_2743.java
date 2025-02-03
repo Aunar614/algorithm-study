01709ea978131f7d501a121f8927940481dfcf29
Aunar614 <developkim614@gmail.com>
2025-02-03 19:48:18 +0530

    BaekJoon_2743

---
+++ src/baekjoon/BaekJoon_2743.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class BaekJoon_2743 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] names = sc.nextLine().split("");
+
+        System.out.println(names.length);
+    }
+}
