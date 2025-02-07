5ee89c16a0e9918db86319c709017ef6d9a918cc
Aunar614 <developkim614@gmail.com>
2025-02-07 19:20:40 +0530

    _4

---
+++ src/programmers/_4.java
@@ -0,0 +1,17 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class _4 {
+
+    public static void main(String[] args) {
+        String[] arr = {"a", "b", "c"};
+
+        String string = String.join("", arr);
+
+    }
+
+    public String solution(String[] arr) {
+        return String.join("", arr);
+    }
+}
