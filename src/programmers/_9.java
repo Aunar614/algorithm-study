aed949f69a36f1c345fedb4c3b90f0db6ac5ebdc
Aunar614 <developkim614@gmail.com>
2025-02-10 19:28:14 +0530

    _9

---
+++ src/programmers/_9.java
@@ -0,0 +1,17 @@
+package programmers;
+
+public class _9 {
+
+    public static void main(String[] args) {
+        int num = 34;
+        int n = 3;
+
+    }
+
+    public int solution(int num, int n) {
+        if (num % n == 0) {
+            return 1;
+        }
+        return 0;
+    }
+}
