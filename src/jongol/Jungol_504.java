2363dc68d02308883c23fc84723035bdb1b570e9
Aunar614 <developkim614@gmail.com>
2025-03-03 18:56:04 +0530

    Jungol_504

---
+++ src/jongol/Jungol_504.java
@@ -0,0 +1,10 @@
+package jongol;
+
+public class Jungol_504 {
+
+    public static void main(String[] args) {
+        System.out.println("(@) (@)");
+        System.out.println("(=^.^=)");
+        System.out.println("(-m-m-)");
+    }
+}
