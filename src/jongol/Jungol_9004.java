55d6b10d7af737a514e4b87bb638586bc2f3f8aa
Aunar614 <developkim614@gmail.com>
2025-03-03 18:54:39 +0530

    Jungol_9004

---
+++ src/jongol/Jungol_9004.java
@@ -0,0 +1,14 @@
+package jongol;
+
+public class Jungol_9004 {
+
+    public static void main(String[] args) {
+        System.out.println("   @@");
+        System.out.println("  @  @");
+        System.out.println(" @    @");
+        System.out.println("@      @");
+        System.out.println(" @    @");
+        System.out.println("  @  @");
+        System.out.println("   @@");
+    }
+}
