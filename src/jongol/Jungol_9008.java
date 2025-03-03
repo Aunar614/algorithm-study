0e0c95a62255ec6229d1766180a6f315e2e408be
Aunar614 <developkim614@gmail.com>
2025-03-03 19:42:04 +0530

    Jungol_9008

---
+++ src/jongol/Jungol_9008.java
@@ -0,0 +1,12 @@
+package jongol;
+
+public class Jungol_9008 {
+
+    public static void main(String[] args) {
+        System.out.println(" subject score");
+        System.out.println("==============");
+        System.out.println("  korean    90");
+        System.out.println(" english   100");
+        System.out.println("computer    80");
+    }
+}
