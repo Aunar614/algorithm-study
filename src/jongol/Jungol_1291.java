3bbd0ddb25f30bcca4f2d3a4dacc06473a03b30f
Aunar614 <developkim614@gmail.com>
2025-03-03 18:44:22 +0530

    Jungol_1291

---
+++ src/jongol/Jungol_1291.java
@@ -0,0 +1,45 @@
+package jongol;
+
+import java.util.Scanner;
+
+public class Jungol_1291 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int s;
+        int e;
+
+        while (true) {
+            s = sc.nextInt();
+            e = sc.nextInt();
+
+            if ((s >= 2 && s <= 9) && (e >= 2 && e <= 9)) {
+                break;
+            } else {
+                System.out.println("INPUT ERROR!");
+            }
+        }
+
+        if (s > e) {
+            for (int i = 1; i <= 9; i++) {
+                for (int j = s; j >= e; j--) {
+                    System.out.printf("%d * %d = %2d", j, i, j * i);
+                    if (j != e) {
+                        System.out.print("   ");
+                    }
+                }
+                System.out.println();
+            }
+        } else {
+            for (int i = 1; i <= 9; i++) {
+                for (int j = s; j <= e; j++) {
+                    System.out.printf("%d * %d = %2d", j, i, j * i);
+                    if (j != e) {
+                        System.out.print("   ");
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
+}
