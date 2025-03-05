f3398d5f2326687668932f9ddb9f81cc99c448d4
Aunar614 <developkim614@gmail.com>
2025-03-05 17:39:46 +0530

    Jungol_9014

---
+++ src/jungol/Jungol_9014.java
@@ -0,0 +1,20 @@
+package jungol;
+
+import java.io.*;
+
+public class Jungol_9014 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("당신의 나이는 몇 살 입니까? ");
+        bw.flush();
+        int age = Integer.parseInt(br.readLine());
+
+        bw.write("당신의 나이는 " + age + "살 이군요.");
+
+        br.close();
+        bw.close();
+    }
+}
