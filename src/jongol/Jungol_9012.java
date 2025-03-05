545c34703cbce19f92db3f2a86313a093745d1b0
Aunar614 <developkim614@gmail.com>
2025-03-05 16:50:52 +0530

    Jungol_9012

---
+++ src/jongol/Jungol_9012.java
@@ -0,0 +1,18 @@
+package jongol;
+
+public class Jungol_9012 {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 2;
+        double circle = 3.140000;
+
+        System.out.printf("원주 = " + a + " * " + b + " * " + "%.6f" + " = ", circle);
+        System.out.printf("%.6f", a * b * circle);
+        System.out.println();
+
+        System.out.printf("넓이 = " + a + " * " + a + " * " + "%.6f" + " = ", circle);
+        System.out.printf("%.6f", a * a * circle);
+    }
+}
