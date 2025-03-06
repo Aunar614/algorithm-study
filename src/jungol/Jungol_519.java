c91d4eb9b5c641bff5d4c2d037359ed70b0b9d23
Aunar614 <developkim614@gmail.com>
2025-03-06 16:04:28 +0530

    Jungol_519

---
+++ src/jungol/Jungol_519.java
@@ -0,0 +1,22 @@
+package jungol;
+
+import java.io.*;
+
+public class Jungol_519 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String number = br.readLine();
+        String[] numbers = number.split(" ");
+
+        int a = Integer.parseInt(numbers[0]) + 100;
+        int b = Integer.parseInt(numbers[1]) % 10;
+
+        bw.write(a + " " + b);
+
+        br.close();
+        bw.close();
+    }
+}
