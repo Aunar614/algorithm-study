a838c6275abd6a5dc6a3593461be392f2507c94f
Aunar614 <developkim614@gmail.com>
2025-03-08 13:44:34 +0530

    Jungol_538

---
+++ src/jungol/Jungol_538.java
@@ -0,0 +1,29 @@
+package jungol;
+
+import java.io.*;
+
+public class Jungol_538 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        while (true) {
+            bw.write("number? ");
+            bw.flush();
+            int number = Integer.parseInt(br.readLine());
+
+            if (number == 0) {
+                break;
+            } else if (number > 0) {
+                bw.write("positive integer\n");
+                bw.flush();
+            } else if (number < 0) {
+                bw.write("negative number\n");
+                bw.flush();
+            }
+        }
+
+        bw.close();
+    }
+}
