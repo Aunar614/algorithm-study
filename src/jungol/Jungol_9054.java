81ba8760264321542fd9f3e3f413f9c49793ee88
Aunar614 <developkim614@gmail.com>
2025-03-10 16:12:50 +0530

    Jungol_9054

---
+++ src/jungol/Jungol_9054.java
@@ -0,0 +1,26 @@
+package jungol;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Jungol_9054 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int max = 9;
+
+        for (int i = 1; i <= max; i++) {
+            for (int j = 2; j <= 4; j++) {
+                bw.write(j + " * " + i + " =" + String.format("%3d", j * i));
+                if (j < 4) {
+                    bw.write("   ");
+                }
+            }
+            bw.write("\n");
+        }
+
+        bw.close();
+    }
+}
