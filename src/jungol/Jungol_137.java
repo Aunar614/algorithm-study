f451b335e6351f445ef4c916b7d41a4e9a401daf
Aunar614 <developkim614@gmail.com>
2025-03-10 16:45:37 +0530

    Jungol_137

---
+++ src/jungol/Jungol_137.java
@@ -0,0 +1,29 @@
+package jungol;
+
+import java.io.*;
+
+public class Jungol_137 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String num = br.readLine();
+        String[] number = num.split(" ");
+
+        int n = Integer.parseInt(number[0]);
+        int m = Integer.parseInt(number[1]);
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                bw.write(j * i + "");
+                if (j < m) {
+                    bw.write(" ");
+                }
+            }
+            bw.write("\n");
+        }
+
+        bw.close();
+    }
+}
