bcaa3753ee1ae67cfda9e01579b42228533e4c39
Aunar614 <developkim614@gmail.com>
2025-03-10 14:53:49 +0530

    Jungol_129

---
+++ src/jungol/Jungol_129.java
@@ -0,0 +1,34 @@
+package jungol;
+
+import java.io.*;
+
+public class Jungol_129 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        while (true) {
+            bw.write("Base = ");
+            bw.flush();
+            int base = Integer.parseInt(br.readLine());
+
+            bw.write("Height = ");
+            bw.flush();
+            int height = Integer.parseInt(br.readLine());
+
+            double triangleWidth = ((double) base * height) / 2;
+            bw.write("Triangle width = " + String.format("%.1f", triangleWidth) + "\n");
+            bw.write("Continue? ");
+            bw.flush();
+
+            String str = br.readLine().toUpperCase();
+            if (!str.equals("Y")) {
+                break;
+            }
+        }
+
+        br.close();
+        bw.close();
+    }
+}
