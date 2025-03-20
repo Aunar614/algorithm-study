969be456edb9af68196cddaf3fbf75091a928b7f
Aunar614 <developkim614@gmail.com>
2025-03-20 16:23:54 +0900

    Jungol_584

---
+++ src/jungol/stepping/function/Jungol_584.java
@@ -0,0 +1,16 @@
+package jungol.stepping.function;
+
+public class Jungol_584 {
+
+    public static void main(String[] args) {
+        define();
+    }
+
+    private static void define() {
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 3; j++) {
+                System.out.println(i + " + " + j + " = " + (i + j));
+            }
+        }
+    }
+}
